8438315bc532d9294f336aade0d64619f85104f1
z2s8 <szanto.zsombor@gmail.com>
2018-10-08 18:11:58 +0200

    Add 3 column grid to about

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,6 +2,22 @@
   <div class="about">
     <h1>This is an about page</h1>
     <br />
+    <div class="row">
+      <div class="col-3">
+        Right sidebar is the right sidebar
+      </div>
+      <div class="col-6">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </div>
+      <div class="col-3">
+        Left sidebar is the left sidebar
+      </div>
+    </div>
     <div class="alert alert-danger" role="alert">
       A simple danger alert—check it out!
     </div>
