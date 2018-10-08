139c3694cb30dfda5717c412a1bd2d1cfdf81d26
z2s8 <szanto.zsombor@gmail.com>
2018-10-08 18:14:54 +0200

    Friendlier style in about:)

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,8 +8,8 @@
       </div>
       <div class="col-6">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
         consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
         cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
         proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -18,8 +18,9 @@
         Left sidebar is the left sidebar
       </div>
     </div>
-    <div class="alert alert-danger" role="alert">
-      A simple danger alert—check it out!
+    <div class="alert alert-info" role="alert">
+      A simple info alert—check it out!
+      It's friendlier than a red alert.
     </div>
   </div>
 </template>
