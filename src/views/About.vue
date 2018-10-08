9682a420ba2937201b5fc11624ebfa59ed1e2134
z2s8 <szanto.zsombor@gmail.com>
2018-10-08 18:05:48 +0200

    Add boostrap and warn in about view

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <br />
+    <div class="alert alert-danger" role="alert">
+      A simple danger alert—check it out!
+    </div>
   </div>
 </template>
