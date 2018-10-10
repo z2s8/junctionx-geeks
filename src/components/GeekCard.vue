d53d8b0237748077f8ea98fd49c995b793d4dbd7
z2s8 <szanto.zsombor@gmail.com>
2018-10-10 10:47:36 +0200

    Create GeekCard component

---
+++ src/components/GeekCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- HTML goes here -->
+  Replace me with content!
+</template>
+
+<script>
+export default {
+  name: 'GeekCard',
+  props: {
+    membername: String,
+    extratext: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+ /* CSS goes here */
+
+</style>
