3506d4f5a0ab7fa15686e5be353f1d17e2dfd048
z2s8 <szanto.zsombor@gmail.com>
2018-10-11 20:38:28 +0200

    [Speedy cars arriving...]

--- server/server.js
+++ server/server.js
@@ -20,3 +20,34 @@ app.get('/counter', (req, res) => {
   counter += 1;
   res.send({'count': counter});
 });
+
+app.get('/cars', (req, res) => {
+  let cars = [
+    {name: "Bugatti", speed: "400 km/h", quickness: "really quick", look: "sporty coupe"},
+    {name: "Audi", speed: "300 km/h", quickness: "kinda okish", look: "casual racer"},
+    {name: "Mercedes", speed: "220 km/h", quickness: "not on point", look: "ugly", recommended: "nope", driver: "Hamilton"},
+    {name: "Lotus", speed: "375 km/h", quickness: "pretty quick", look: "sporty beast"},
+    {name: "Bentley", speed: "190 km/h", quickness: "not that speedy", look: "the king of town", price: "90k EUR"}
+  ];
+  res.send(shuffle(cars));
+});
+
+function shuffle(array) {
+    let counter = array.length;
+
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        let index = Math.floor(Math.random() * counter);
+
+        // Decrease counter by 1
+        counter--;
+
+        // And swap the last element with it
+        let temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+
+    return array;
+}
---
+++ src/views/Cars.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <h2>These are my cars</h2>
+    <div v-for="car in cars">
+        <div v-for="(item, key, index) in car" class="row">
+          <div class="col-6 title">
+            {{ key }}:  
+          </div>
+          <div class="col-6 value">
+           {{ item }}
+          </div>
+        </div>
+      <hr/>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  const axios = require('axios');
+
+  export default {
+    data () {
+      return {
+        cars: [],
+      }
+    },
+
+    mounted() {
+      console.log('Vroom Vroom [cars arriving...]');
+      axios
+        .get('http://localhost:9090/cars')
+        .then(response => (this.cars = response.data))
+    }
+  }
+</script>
+
+<style scoped>
+  ul {
+    padding-bottom: 20px;
+  }
+  .title {
+    text-align: right;
+  }
+  .value {
+    text-align: left;
+  }
+</style>
